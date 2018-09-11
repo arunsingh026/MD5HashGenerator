0d172f9a43ee76e20bb50853234d8f15ae6219c8
arun.singh <arun.singh@sonata-software.com>
2018-09-11 10:54:35 +0530

    Add project files.

---
+++ MD5HashGenerator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MD5HashGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
